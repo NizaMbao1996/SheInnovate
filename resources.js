3d840546e6c0d9dfcc3dbc13e3edfbc28a501d50
Niza Mbao <116558653+NizaMbao1996@users.noreply.github.com>
2025-04-12 23:40:46 +0200

    Delete resources.js

--- resources.js
+++
@@ -1,4 +0,0 @@
-// JavaScript functionality for the resources section
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Resources JavaScript loaded for SheInnovate.');
-});
